5a871d15b3d5da1b7600348c218f4eef494c3528
threadhack <threadhack@gmail.com>
2012-08-02 14:54:33 -0700

    A very simple unit test with .NET

---
+++ src/MyCalc.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace QuickTesting
+{
+    /// <summary>
+    /// Very simple class for the purpose of testing
+    /// </summary>
+    public class MyCalc
+    {
+        /// <summary>
+        /// Adds two numbers
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public virtual int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        /// <summary>
+        /// Subtracts b from a
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public virtual int Sub(int a, int b)
+        {
+            return a - b;
+        }
+
+        /// <summary>
+        /// Multiplies a times b
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public virtual decimal Mul(int a, int b)
+        {
+            return a * b;
+        }
+
+        /// <summary>
+        /// Divides a by b
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public virtual decimal Div(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
---
+++ test/MyCalcTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QuickTesting;
+
+namespace QuickTestingUnitTests
+{
+    [TestClass()]
+    public class MyCalcTest
+    {
+        private MyCalc myCalc = new MyCalc();
+
+        /// <summary>
+        /// Test the Add function
+        /// </summary>
+        [TestMethod()]
+        public void TestAdd()
+        {
+            Assert.AreEqual(this.myCalc.Add(1, 1), 2);
+        }
+
+        /// <summary>
+        /// Test the Subtract function
+        /// </summary>
+        [TestMethod()]
+        public void TestSub()
+        {
+            Assert.AreEqual(this.myCalc.Sub(3, 2), 1);
+        }
+
+        /// <summary>
+        /// Test the Multiply function
+        /// </summary>
+        [TestMethod()]
+        public void TestMul()
+        {
+            Assert.AreEqual(this.myCalc.Mul(3, 2), 6);
+        }
+
+        /// <summary>
+        /// Test the Division function
+        /// </summary>
+        [TestMethod()]
+        public void TestDiv()
+        {
+            Assert.AreEqual(this.myCalc.Div(3, 1), 3);
+        }
+
+        /// <summary>
+        /// Test that an expected exception is obtained
+        /// </summary>
+        [TestMethod()]
+        [ExpectedException(typeof(DivideByZeroException))]
+        public void TestDivWithException()
+        {
+            Assert.AreEqual(this.myCalc.Div(3, 0), 1);
+        }
+    }
+}
